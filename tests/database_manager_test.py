5c8a5dc0246a858e34f047399f185d1025ea3573
LorincziMatyas <lorinczi.matyas@student.ms.sapientia.ro>
2024-03-06 19:35:51 +0200

    modified test file

--- tests/database_manager_test.py
+++ tests/database_manager_test.py
@@ -10,6 +10,8 @@ if db_manager.get_all_employees() is None:
 
 # Check if there is a team leader called John Doe whose birthdate is 31.01.1970.
 def test_john_doe_is_team_leader():
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     john = [e for e in employees if e.first_name == "John" and e.last_name == "Doe"]
@@ -21,6 +23,8 @@ def test_john_doe_is_team_leader():
 
 # Check if John Doe’s team members are Myrta Torkelson and Jettie Lynch.
 def test_john_doe_team_members():
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     john = [e for e in employees if e.first_name == "John" and e.last_name == "Doe"]
@@ -35,6 +39,8 @@ def test_john_doe_team_members():
 
 # Make sure that Tomas Andre is not John Doe’s team member.
 def test_tomas_andre_not_in_john_doe_team():
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     john = [e for e in employees if e.first_name == "John" and e.last_name == "Doe"]
@@ -47,6 +53,8 @@ def test_tomas_andre_not_in_john_doe_team():
 
 # Check if Gretchen Walford’s base salary equals 4000$.
 def test_gretchen_watford_base_salary():
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     gretchen = [
@@ -59,6 +67,8 @@ def test_gretchen_watford_base_salary():
 
 # Make sure Tomas Andre is not a team leader. Check what happens if you try to retrieve his team members.
 def test_tomas_andre_not_team_leader():
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     tomas = [e for e in employees if e.first_name == "Tomas" and e.last_name == "Andre"]
@@ -69,6 +79,8 @@ def test_tomas_andre_not_team_leader():
 
 # Make sure that Jude Overcash is not stored in the database.
 def test_jude_overcash_not_in_database():
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     jude = [
@@ -80,6 +92,8 @@ def test_jude_overcash_not_in_database():
 # Check an employee’s salary who is not a team leader whose hire date is 10.10.1998
 # and his base salary is 1000$. Make sure the returned value is 3000$ (1000$ + 20 X 100$).
 def test_employee_salary_without_leadership_bonus():  # should fail because of the wrong date
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     not_leaders = [
@@ -100,6 +114,8 @@ def test_employee_salary_without_leadership_bonus():  # should fail because of t
 # She was hired on 10.10.2008 and has a base salary of 2000$.
 # Validate if the returned value is 3600$ (2000$ + 10 X 100$ + 3 X 200$).
 def test_leader_salary_with_team_bonus():  # should fail because of the wrong date
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     leaders = [
@@ -122,6 +138,8 @@ def test_leader_salary_with_team_bonus():  # should fail because of the wrong da
 # the respective email sender service is used with the correct information (name and message).
 # You can use the setup from the previous test for the employee.
 def test_check_email_sender():
+    if db_manager.get_all_employees() is None:
+        db_manager.topup_database()
     employees = db_manager.get_all_employees()
     assert employees is not None
     salary, name, message = db_manager.calculate_salary_and_send_email_modified(
