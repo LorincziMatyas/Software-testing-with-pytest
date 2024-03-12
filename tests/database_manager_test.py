d070fb12f0ee3cdb6f0f8e0ec62c7141b1a9cd2a
LorincziMatyas <lorinczi.matyas@student.ms.sapientia.ro>
2024-03-12 11:12:05 +0200

    Modified commet to docstring, trimed whitespaces. added the venv to gitignore

--- tests/database_manager_test.py
+++ tests/database_manager_test.py
@@ -3,9 +3,8 @@ from src.database_manager import DatabaseManager
 from tests import db_manager
 
 
-
 def test_john_doe_is_team_leader():
-    ''' Check if there is a team leader called John Doe whose birthdate is 31.01.1970.'''    
+    """Check if there is a team leader called John Doe whose birthdate is 31.01.1970."""
     employees = db_manager.get_all_employees()
     assert employees is not None
     john = [e for e in employees if e.first_name == "John" and e.last_name == "Doe"]
@@ -15,9 +14,8 @@ def test_john_doe_is_team_leader():
     assert db_manager.is_leader(john)
 
 
-
 def test_john_doe_team_members():
-    '''Check if John Doe’s team members are Myrta Torkelson and Jettie Lynch.''' 
+    """Check if John Doe’s team members are Myrta Torkelson and Jettie Lynch."""
     employees = db_manager.get_all_employees()
     assert employees is not None
     john = [e for e in employees if e.first_name == "John" and e.last_name == "Doe"]
@@ -30,8 +28,8 @@ def test_john_doe_team_members():
     assert "Jettie Lynch" in member_names
 
 
-# Make sure that Tomas Andre is not John Doe’s team member.
 def test_tomas_andre_not_in_john_doe_team():
+    """Make sure that Tomas Andre is not John Doe’s team member."""
     employees = db_manager.get_all_employees()
     assert employees is not None
     john = [e for e in employees if e.first_name == "John" and e.last_name == "Doe"]
@@ -42,8 +40,8 @@ def test_tomas_andre_not_in_john_doe_team():
     assert "Tomas Andre" not in member_names
 
 
-# Check if Gretchen Walford’s base salary equals 4000$.
 def test_gretchen_watford_base_salary():
+    """Check if Gretchen Walford’s base salary equals 4000$."""
     employees = db_manager.get_all_employees()
     assert employees is not None
     gretchen = [
@@ -54,8 +52,8 @@ def test_gretchen_watford_base_salary():
     assert gretchen.base_salary == 4000
 
 
-# Make sure Tomas Andre is not a team leader. Check what happens if you try to retrieve his team members.
 def test_tomas_andre_not_team_leader():
+    """Make sure Tomas Andre is not a team leader. Check what happens if you try to retrieve his team members."""
     employees = db_manager.get_all_employees()
     assert employees is not None
     tomas = [e for e in employees if e.first_name == "Tomas" and e.last_name == "Andre"]
@@ -64,8 +62,8 @@ def test_tomas_andre_not_team_leader():
     assert not db_manager.is_leader(tomas)
 
 
-# Make sure that Jude Overcash is not stored in the database.
 def test_jude_overcash_not_in_database():
+    """Make sure that Jude Overcash is not stored in the database."""
     employees = db_manager.get_all_employees()
     assert employees is not None
     jude = [
@@ -74,9 +72,9 @@ def test_jude_overcash_not_in_database():
     assert len(jude) == 0
 
 
-# Check an employee’s salary who is not a team leader whose hire date is 10.10.1998
-# and his base salary is 1000$. Make sure the returned value is 3000$ (1000$ + 20 X 100$).
 def test_employee_salary_without_leadership_bonus():
+    """# Check an employee’s salary who is not a team leader whose hire date is 10.10.1998 and his base
+    salary is 1000$. Make sure the returned value is 3000$ (1000$ + 20 X 100$)."""
     employees = db_manager.get_all_employees()
     assert employees is not None
     not_leaders = [
@@ -93,10 +91,9 @@ def test_employee_salary_without_leadership_bonus():
     # assert calculated_salary != expected_salary
 
 
-# Check an employee’s salary who is a team leader and his team consists of 3 members.
-# She was hired on 10.10.2008 and has a base salary of 2000$.
-# Validate if the returned value is 3600$ (2000$ + 10 X 100$ + 3 X 200$).
 def test_leader_salary_with_team_bonus():
+    """Check a team leader’s salary whose hire date is 10.10.2008 and his base salary is 2000$.
+    Make sure the returned value is 3600$ (2000$ + 3 X 200$)."""
     employees = db_manager.get_all_employees()
     assert employees is not None
     leaders = [
@@ -115,10 +112,10 @@ def test_leader_salary_with_team_bonus():
     # assert calculated_salary == expected_salary
 
 
-# Make sure that when you calculate the salary and send an email notification,
-# the respective email sender service is used with the correct information (name and message).
-# You can use the setup from the previous test for the employee.
 def test_check_email_sender():
+    """Make sure that when you calculate the salary and send an email notification,
+    the respective email sender service is used with the correct information (name and message).
+    You can use the setup from the previous test for the employee."""
     db_manager = DatabaseManager()
     if db_manager.get_all_employees() is None:
         db_manager.topup_database()
@@ -130,6 +127,3 @@ def test_check_email_sender():
     assert salary == 6800
     assert name == "John Doe"
     assert message == "John Doe, your salary: 6800 has been transferred to you."
-
-
-# db_manager.clear_database()
